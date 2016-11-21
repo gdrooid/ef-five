5357d7f1d702e5aa696dd44b9ac221aa345c2780
Daniel Rodriguez <gdrooid@openmailbox.org>
2016-11-21 19:20:12 +0100

    Add code

---
+++ background-f5.js
@@ -0,0 +1,38 @@
+var tabs = {};
+
+function addTab(tabId, delay) {
+  if (tabId in tabs) return;
+
+  var intervalId = window.setInterval(() => {
+    browser.tabs.reload(tabId);
+  }, delay);
+
+  tabs[tabId] = intervalId;
+}
+
+function removeTab(tabId) {
+  if (!(tabId in tabs)) return;
+
+  window.clearInterval(tabs[tabId]);
+
+  delete tabs[tabId];
+}
+
+browser.runtime.onMessage.addListener((msg, _, sendResponse) => {
+  switch (msg.type) {
+    case 'add':
+      addTab(msg.tabId, msg.delay);
+      break;
+
+    case 'remove':
+      removeTab(msg.tabId);
+      break;
+
+    case 'status':
+      sendResponse({
+        type: 'statusResponse',
+        reloading: (msg.tabId in tabs)
+      });
+      break;
+  }
+});
---
+++ f5.js
@@ -0,0 +1,73 @@
+function addTab(tabId, delay) {
+  browser.runtime.sendMessage({
+    type: 'add',
+    tabId: tabId,
+    delay: delay
+  });
+}
+
+function removeTab(tabId) {
+  browser.runtime.sendMessage({
+    type: 'remove',
+    tabId: tabId
+  });
+}
+
+function loadStatus(tabId) {
+  browser.runtime.sendMessage({
+    type: 'status',
+    tabId: tabId
+  }).then((msg) => {
+    if (msg.type !== 'statusResponse') return;
+
+    if (msg.reloading) showReloading();
+    else showNotReloading();
+  });
+}
+
+function elem(id) {
+  return document.getElementById(id);
+}
+
+function showReloading() {
+  elem('input-delay').style.display = 'none';
+  elem('btn-add').style.display = 'none';
+  elem('btn-remove').style.display = '';
+}
+
+function showNotReloading() {
+  elem('input-delay').style.display = '';
+  elem('btn-add').style.display = '';
+  elem('btn-remove').style.display = 'none';
+}
+
+document.addEventListener('click', (e) => {
+  var activeTab = browser.tabs.query({
+    active: true,
+    currentWindow: true
+  });
+
+  if (e.target.id === 'btn-add') {
+    var delay = elem('input-delay').value;
+    if (delay > 0) {
+      activeTab.then((tabs) => {
+        addTab(tabs[0].id, delay);
+        showReloading();
+      });
+    }
+  }
+
+  else if (e.target.id === 'btn-remove') {
+    activeTab.then((tabs) => {
+      removeTab(tabs[0].id);
+      showNotReloading();
+    });
+  }
+});
+
+browser.tabs.query({
+    active: true,
+    currentWindow: true
+}).then((tabs) => {
+  loadStatus(tabs[0].id);
+});
